aa565d4c160bc645c5ceaa021309d9a02fb9abec
developebyjk <developedbyjk@gmail.com>
2023-10-25 08:32:51 +0530

    improve app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import { BrowserRouter } from 'react-router-dom'
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars,faXmark } from "@fortawesome/free-solid-svg-icons";
-import {NavLink} from 'react-router-dom'
+
 import './App.css'
 
 function App() {
